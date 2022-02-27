29cdcee2bd89123f8a5341ff8cbdb49a7eadf2b2
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2022-02-27 10:06:29 -0300

    Estudo

---
+++ BancoDeDados/mongo-retrieve.js
@@ -0,0 +1,8 @@
+const mongoist =  require('mongoist');
+const db = mongoist('mongodb://localhost:27017/livro_nodejs');
+
+db.stormtroopers.find()
+    .then(result => {
+        console.log( result );
+        process.exit();
+    });
